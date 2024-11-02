0aaf34139c7961feac38be2a77d4c91ef0bda875
Thien Pham <thien111548@gmail.com>
2024-11-02 15:13:44 +0700

    Update WebTableTest.java

--- src/test/java/theInternet/WebTableTest.java
+++ src/test/java/theInternet/WebTableTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;

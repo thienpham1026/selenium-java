94c68892981ef5fac036447bed3aa6979ffcab75
Thien Pham <thien111548@gmail.com>
2024-11-04 00:02:36 +0700

    code change

---
+++ src/test/java/theInternet/BrowserUtils.java
@@ -0,0 +1,40 @@
+public class BrowserUtils {
+    private static WebDriver driver;
+    private static Actions mouse;
+    private static WebDriverWait wait;
+
+    public static void launch(String name) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Browser name must not be null or empty");
+        }
+
+        switch (name.toLowerCase()) {
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.setHeadless(true);
+                driver = new ChromeDriver(chromeOptions);
+                break;
+
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+
+            case "ie":
+                WebDriverManager.iedriver().setup();
+                driver = new InternetExplorerDriver();
+                break;
+
+            default:
+                throw new IllegalArgumentException("Unsupported browser: " + name);
+        }
+
+        mouse = new Actions(driver);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+    }
+
+    public static WebDriver getDriver() {
+        return driver;
+    }
+}

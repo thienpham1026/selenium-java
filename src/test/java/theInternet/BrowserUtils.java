b24b8dbca5e18877d7dbbc4fc562a3b913024799
Thien Pham <thien111548@gmail.com>
2024-11-04 00:36:54 +0700

    save code

--- src/test/java/theInternet/BrowserUtils.java
+++ src/test/java/theInternet/BrowserUtils.java
@@ -1,54 +1,54 @@
-package theInternet;
+// package theInternet;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+// import org.openqa.selenium.WebDriver;
+// import org.openqa.selenium.chrome.ChromeDriver;
+// import org.openqa.selenium.chrome.ChromeOptions;
+// import org.openqa.selenium.firefox.FirefoxDriver;
+// import org.openqa.selenium.edge.EdgeDriver;
+// import org.openqa.selenium.interactions.Actions;
+// import org.openqa.selenium.support.ui.WebDriverWait;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+// import io.github.bonigarcia.wdm.WebDriverManager;
 
-import java.time.Duration;
+// import java.time.Duration;
 
-public class BrowserUtils {
-    private static WebDriver driver;
-    private static Actions mouse;
-    private static WebDriverWait wait;
+// public class BrowserUtils {
+//     private static WebDriver driver;
+//     private static Actions mouse;
+//     private static WebDriverWait wait;
 
-    public static void launch(String name) {
-        if (name == null || name.isEmpty()) {
-            throw new IllegalArgumentException("Browser name must not be null or empty");
-        }
+//     public static void launch(String name) {
+//         if (name == null || name.isEmpty()) {
+//             throw new IllegalArgumentException("Browser name must not be null or empty");
+//         }
 
-        switch (name.toLowerCase()) {
-            case "chrome":
-                WebDriverManager.chromedriver().setup();
-                ChromeOptions chromeOptions = new ChromeOptions();
-                chromeOptions.setHeadless(true);
-                driver = new ChromeDriver(chromeOptions);
-                break;
+//         switch (name.toLowerCase()) {
+//             case "chrome":
+//                 WebDriverManager.chromedriver().setup();
+//                 ChromeOptions chromeOptions = new ChromeOptions();
+//                 chromeOptions.setHeadless(true);
+//                 driver = new ChromeDriver(chromeOptions);
+//                 break;
 
-            case "firefox":
-                WebDriverManager.firefoxdriver().setup();
-                driver = new FirefoxDriver();
-                break;
+//             case "firefox":
+//                 WebDriverManager.firefoxdriver().setup();
+//                 driver = new FirefoxDriver();
+//                 break;
 
-            case "edge":
-                WebDriverManager.edgedriver().setup();
-                driver = new EdgeDriver();
-                break;
+//             case "edge":
+//                 WebDriverManager.edgedriver().setup();
+//                 driver = new EdgeDriver();
+//                 break;
 
-            default:
-                throw new IllegalArgumentException("Unsupported browser: " + name);
-        }
+//             default:
+//                 throw new IllegalArgumentException("Unsupported browser: " + name);
+//         }
 
-        mouse = new Actions(driver);
-        wait = new WebDriverWait(driver, Duration.ofSeconds(30));
-    }
+//         mouse = new Actions(driver);
+//         wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+//     }
 
-    public static WebDriver getDriver() {
-        return driver;
-    }
-}
+//     public static WebDriver getDriver() {
+//         return driver;
+//     }
+// }

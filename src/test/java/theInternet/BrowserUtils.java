d623d8ff35f1ad63c5a201641db277e53f8a1f85
Thien Pham <thien111548@gmail.com>
2024-11-04 00:03:37 +0700

    save ode

--- src/test/java/theInternet/BrowserUtils.java
+++ src/test/java/theInternet/BrowserUtils.java
@@ -1,3 +1,13 @@
+package theInternet;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 public class BrowserUtils {
     private static WebDriver driver;
     private static Actions mouse;

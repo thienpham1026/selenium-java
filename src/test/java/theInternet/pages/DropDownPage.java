91c95e1aee1d9ac1750864b2641d6c31165d8fd1
Thien Pham <thien111548@gmail.com>
2024-11-23 14:32:59 +0700

    save code

--- src/test/java/theInternet/pages/DropDownPage.java
+++ src/test/java/theInternet/pages/DropDownPage.java
@@ -9,11 +9,6 @@ public class DropDownPage {
     //WebDriver driver;
     Select select;
 
-    public DropDownPage() {
-        //this.driver = Browser.getDriver();
-        //select = new Select(Browser.findElement(By.id("fruits")));
-    }
-
     public void open(String url) {
         Browser.visit(url);
     }
@@ -28,24 +23,24 @@ public class DropDownPage {
     }
 
     public boolean selectedMultiOption() {
-        select = new Select(driver.findElement(By.id("fruits")));
+        select = new Select(Browser.findElement(By.id("fruits")));
         return select.isMultiple();
     }
 
     public void selectMultiOption(String option1, String option2) {
-        select = new Select(driver.findElement(By.id("fruits")));
+        select = new Select(Browser.findElement(By.id("fruits")));
 
         select.selectByVisibleText(option1);
         select.selectByVisibleText(option2);
     }
 
     public void deSelectOption(String option) {
-        select = new Select(driver.findElement(By.id("fruits")));
+        select = new Select(Browser.findElement(By.id("fruits")));
         select.deselectByVisibleText(option);
     }
 
     public void deSelectAll() {
-        select = new Select(driver.findElement(By.id("fruits")));
+        select = new Select(Browser.findElement(By.id("fruits")));
         select.deselectAll();
     }
 

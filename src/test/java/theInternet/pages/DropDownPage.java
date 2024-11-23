490f8b2f4a48f97c83e3fb9b2b82a621369c39c4
Thien Pham <thien111548@gmail.com>
2024-11-23 14:21:45 +0700

    save code change

--- src/test/java/theInternet/pages/DropDownPage.java
+++ src/test/java/theInternet/pages/DropDownPage.java
@@ -6,10 +6,12 @@ import org.openqa.selenium.support.ui.Select;
 import supports.Browser;
 
 public class DropDownPage {
-    WebDriver driver;
+    //WebDriver driver;
+    Select select;
 
     public DropDownPage() {
-        this.driver = Browser.getDriver();
+        //this.driver = Browser.getDriver();
+        select = new Select(Browser.findElement(By.id("fruits")));
     }
 
     public void open(String url) {
@@ -17,37 +19,37 @@ public class DropDownPage {
     }
 
     public void selectOption(String option) {
-        Select dropdown = new Select(driver.findElement(By.xpath("//select[@id='dropdown']")));
+        Select dropdown = new Select(Browser.findElement(By.xpath("//select[@id='dropdown']")));
         dropdown.selectByVisibleText(option);
     }
 
     public boolean isSelected(String option) {
-        return driver.findElement(By.xpath(String.format("//*[@id='dropdown']/option[.='%s']", option))).isSelected();
+        return Browser.findElement(By.xpath(String.format("//*[@id='dropdown']/option[.='%s']", option))).isSelected();
     }
 
     public boolean selectedMultiOption() {
-        Select select = new Select(driver.findElement(By.id("fruits")));
+        //Select select = new Select(driver.findElement(By.id("fruits")));
         return select.isMultiple();
     }
 
     public void selectMultiOption(String option1, String option2) {
-        Select select = new Select(driver.findElement(By.id("fruits")));
+        //Select select = new Select(driver.findElement(By.id("fruits")));
 
         select.selectByVisibleText(option1);
         select.selectByVisibleText(option2);
     }
 
     public void deSelectOption(String option) {
-        Select select = new Select(driver.findElement(By.id("fruits")));
+        //Select select = new Select(driver.findElement(By.id("fruits")));
         select.deselectByVisibleText(option);
     }
 
     public void deSelectAll() {
-        Select select = new Select(driver.findElement(By.id("fruits")));
+        //Select select = new Select(driver.findElement(By.id("fruits")));
         select.deselectAll();
     }
 
     public boolean isMultiSelected(String option) {
-        return driver.findElement(By.xpath(String.format("//*[@id='fruits']/option[@value='%s']", option))).isSelected();
+        return Browser.findElement(By.xpath(String.format("//*[@id='fruits']/option[@value='%s']", option))).isSelected();
     }
 }

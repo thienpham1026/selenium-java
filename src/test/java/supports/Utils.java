aa4dbb248516982e4dcfee18297a13c35847da86
Thien Pham <thien111548@gmail.com>
2024-11-25 00:27:57 +0700

    Create Utils.java

---
+++ src/test/java/supports/Utils.java
@@ -0,0 +1,20 @@
+package supports;
+
+import java.util.Random;
+
+public class Utils {
+
+    // dynamic test data generator
+    public static String generateRandomString(int length) {
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        Random random = new Random();
+        StringBuilder result = new StringBuilder(length);
+
+        for (int i = 0; i < length; i++) {
+            int index = random.nextInt(characters.length());
+            result.append(characters.charAt(index));
+        }
+
+        return result.toString();
+    }
+}

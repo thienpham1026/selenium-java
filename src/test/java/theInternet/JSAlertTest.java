3950e19a259a003f8dfef8b9b1d806a34575d749
Pham Thanh Thien <thien.pt@mekongpetro.com>
2024-12-19 17:40:58 +0700

    save code change

--- src/test/java/theInternet/JSAlertTest.java
+++ src/test/java/theInternet/JSAlertTest.java
@@ -6,53 +6,53 @@ import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-import supports.Browser;
+import static supports.Browser.*;
 
 public class JSAlertTest {
     WebDriver driver;
 
     @BeforeClass
     void setUp() {
-        Browser.openBrowser("chrome");
-        driver = Browser.getDriver();
-        Browser.visit("https://the-internet.herokuapp.com/javascript_alerts");
+        openBrowser("chrome");
+        driver = getDriver();
+        visit("https://the-internet.herokuapp.com/javascript_alerts");
     }
 
     @Test
     void clickJsAlertSuccessfully() {
-        driver.findElement(By.xpath("//button[.='Click for JS Alert']")).click();
+        getElement(By.xpath("//button[.='Click for JS Alert']")).click();
         driver.switchTo().alert().accept();
 
-        Assert.assertEquals(driver.findElement(By.id("result")).getText(), "You successfully clicked an alert");
+        Assert.assertEquals(getText(By.id("result")), "You successfully clicked an alert");
     }
 
     @Test
     void verifyClickOKinJsAlertConfirmSuccessfully() {
-        driver.findElement(By.xpath("//button[.='Click for JS Confirm']")).click();
+        getElement(By.xpath("//button[.='Click for JS Confirm']")).click();
         driver.switchTo().alert().accept();
 
-        Assert.assertEquals(driver.findElement(By.id("result")).getText(), "You clicked: Ok");
+        Assert.assertEquals(getText(By.id("result")), "You clicked: Ok");
     }
 
     @Test
     void verifyClickCancelInJsAlertConfirmSuccessfully() {
-        driver.findElement(By.xpath("//button[.='Click for JS Confirm']")).click();
+        getElement(By.xpath("//button[.='Click for JS Confirm']")).click();
         driver.switchTo().alert().dismiss();
-        Assert.assertEquals(driver.findElement(By.id("result")).getText(), "You clicked: Cancel");
+        Assert.assertEquals(getText(By.id("result")), "You clicked: Cancel");
     }
 
     @Test
     void verifyInputJsAlertPromptSuccessfully() {
-        driver.findElement(By.xpath("//button[.='Click for JS Prompt']")).click();
+        getElement(By.xpath("//button[.='Click for JS Prompt']")).click();
 
         driver.switchTo().alert().sendKeys("hello");
         driver.switchTo().alert().accept();
 
-        Assert.assertEquals(driver.findElement(By.id("result")).getText(), "You entered: hello");
+        Assert.assertEquals(getText(By.id("result")), "You entered: hello");
     }
 
     @AfterClass
     void tearDown() {
-        Browser.quit();
+        quit();
     }
 }
--- src/test/java/theInternet/NestedFrameTest.java
+++ src/test/java/theInternet/NestedFrameTest.java
@@ -6,39 +6,39 @@ import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-import supports.Browser;
+import static supports.Browser.*;
 
 public class NestedFrameTest {
 
     @BeforeClass
     void setUp() {
-        Browser.openBrowser("chrome");
+        openBrowser("chrome");
     }
 
     @Test
     void verifyFrameContent() {
-        WebDriver driver = Browser.getDriver();
-        driver.get("https://the-internet.herokuapp.com/nested_frames");
+        WebDriver driver = getDriver();
+        visit("https://the-internet.herokuapp.com/nested_frames");
 
         driver.switchTo().frame("frame-top");
         driver.switchTo().frame("frame-left");
-        Assert.assertTrue(driver.findElement(By.tagName("body")).getText().contains("LEFT"));
+        Assert.assertTrue(getText(By.tagName("body")).contains("LEFT"));
 
         driver.switchTo().parentFrame();
         driver.switchTo().frame("frame-middle");
-        Assert.assertTrue(driver.findElement(By.id("content")).getText().contains("MIDDLE"));
+        Assert.assertTrue(getText(By.id("content")).contains("MIDDLE"));
 
         driver.switchTo().parentFrame();
         driver.switchTo().frame("frame-right");
-        Assert.assertTrue(driver.findElement(By.tagName("body")).getText().contains("RIGHT"));
+        Assert.assertTrue(getText(By.tagName("body")).contains("RIGHT"));
 
         driver.switchTo().defaultContent();
         driver.switchTo().frame("frame-bottom");
-        Assert.assertTrue(driver.findElement(By.tagName("body")).getText().contains("BOTTOM"));
+        Assert.assertTrue(getText(By.tagName("body")).contains("BOTTOM"));
     }
 
     @AfterClass
     void tearDown() {
-        Browser.quit();
+        quit();
     }
 }

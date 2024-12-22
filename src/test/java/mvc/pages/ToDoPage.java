b6c7d00b9b2f37fd8910b96610be18559d49d4ef
Pham Thanh Thien <thien.pt@mekongpetro.com>
2024-12-22 16:42:47 +0700

    save code change

--- src/test/java/mvc/pages/ToDoPage.java
+++ src/test/java/mvc/pages/ToDoPage.java
@@ -10,41 +10,42 @@ import java.util.List;
 import static supports.Browser.*;
 
 public class ToDoPage {
-    public void open(){
+
+    public void open() {
         visit("https://todomvc.com/examples/react/dist/#/");
     }
 
-    public void addNewTask(String taskName){
-        fill(By.className("new-todo"),taskName+"\n");
+    public void addNewTask(String taskName) {
+        fill(By.className("new-todo"), taskName + "\n");
     }
 
-    public void markComplete(String taskName){
+    public void markComplete(String taskName) {
         getTask(taskName).findElement(By.tagName("input")).click();
     }
 
-    public void delete(String taskName){
+    public void delete(String taskName) {
         hover(getTask(taskName));
         getTask(taskName).findElement(By.tagName("button")).click();
     }
 
-    public void editTaskName(String taskName,String newName){
+    public void editTaskName(String taskName, String newName) {
         WebElement task = getTask(taskName);
 
         doubleClick(task);
         WebElement editTaskNameTextBox = task.findElement(By.cssSelector("input.new-todo"));
-        executeScript(editTaskNameTextBox,"arguments[0].value = ''");
-        editTaskNameTextBox.sendKeys(newName,Keys.ENTER);
+        executeScript(editTaskNameTextBox, "arguments[0].value = ''");
+        editTaskNameTextBox.sendKeys(newName, Keys.ENTER);
     }
 
-    public boolean isTaskExist(String taskName){
+    public boolean isTaskExist(String taskName) {
         List<WebElement> tasks = all(By.cssSelector(".todo-list li"));
-        if (tasks.isEmpty())  return  false;
+        if (tasks.isEmpty()) return false;
 
-        String lastTaskName=  tasks.get(tasks.size()-1).getText();
+        String lastTaskName = tasks.get(tasks.size() - 1).getText();
         return taskName.contains(lastTaskName);
     }
 
-    public WebElement getTask(String taskName){
+    public WebElement getTask(String taskName) {
         List<WebElement> tasks = all(By.cssSelector(".todo-list li"));
         return tasks.stream()
                 .filter(task -> task.getText().equals(taskName))
@@ -52,12 +53,12 @@ public class ToDoPage {
                 .get();
     }
 
-    public boolean isTaskCompleted(String taskName){
+    public boolean isTaskCompleted(String taskName) {
         return getTask(taskName).getAttribute("class").contains("completed");
     }
 
-    public int getItemsLeft(){
-        if(all(By.className("todo-count")).isEmpty()) return 0;
+    public int getItemsLeft() {
+        if (all(By.className("todo-count")).isEmpty()) return 0;
 
         String itemsLeftContext = Browser.getText(By.className("todo-count"));
         return Integer.parseInt(itemsLeftContext.split(" ")[0]);
